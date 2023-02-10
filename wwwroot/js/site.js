19c9e14e55e4dfabcc30a44ab5932a4b681d3c02
Алексей Манаев <manaev_001@mail.ru>
2023-02-11 00:10:07 +0300

    Add project files.

---
+++ wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿window.addEventListener("scroll", e => {
+    document.body.style.cssText += `--scrollTop: ${this.scrollY}px`
+})
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+gsap.config({ trialWarn: false });
+
+ScrollSmoother.create({
+    wrapper: '.wrapper',
+    content: '.container'
+})

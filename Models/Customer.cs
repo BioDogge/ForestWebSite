89e01885f96e75ef7ef191af23512ad03e608107
Алексей Манаев <manaev_001@mail.ru>
2023-02-14 21:33:51 +0300

    Added validation.

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -7,15 +7,10 @@ namespace ForestWebSite.Models
 		[Key]
 		public int Id { get; set; }
 
-		//[Required]
 		public string Name { get; set; } = string.Empty;
 
-		//[Required]
-		//[RegularExpression(@"^\+7-\d{3}-\d{3}-\d{4}$", ErrorMessage = "Phone Number must match the '+7-xxx-xxx-xxxx' template")]
 		public string Phone { get; set; } = string.Empty;
 
-		//[Required]
-		//[RegularExpression(@"(\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*)", ErrorMessage = "Email address is incorrect")]
 		public string Email { get; set; } = string.Empty;
 
 		public DateTime CreatedDateTime { get; set; }
